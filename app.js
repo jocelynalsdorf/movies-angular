0e1a1d3c70ee13b65350beba0857fd8106783aec
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-03 11:49:23 -0700

    add header templating for site

--- app.js
+++ app.js
@@ -4,19 +4,40 @@ var app = angular.module("movieApp", ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider.state('home', {
     url: "",
-    templateUrl: "partials/home.html"
+    views: {
+      "header": {
+        templateUrl: "partials/header.html"
+      },
+       "body": {
+         templateUrl: "partials/home.html"
+       },
+    }
   });
 
   $stateProvider.state('movies', {
     url: "/movies",
-    templateUrl: "partials/movies.html",
-    controller: "MoviesCtrl"
+     views: {
+      "header": {
+        templateUrl: "partials/header.html"
+      },
+      "body": {
+        templateUrl: "partials/movies.html",
+        controller: "MoviesCtrl"
+       },
+    }
   });
 
   $stateProvider.state('movies.reviews', {
     url: "/:movieId",
-    templateUrl: "partials/movies.reviews.html",
-    controller: "ReviewsCtrl"
+    views: {
+      "header": {
+        templateUrl: "partials/header.html"
+      },
+      "body": {
+        templateUrl: "partials/movies.reviews.html",
+        controller: "ReviewsCtrl"
+       },
+    }
   });
 
 });
